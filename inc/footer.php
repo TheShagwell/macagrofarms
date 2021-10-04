6ce57483db7e788dca8fa1bbdfa852230882d697
shagwell <kelvinerewharighe@gmail.com>
2021-10-04 13:21:02 +0100

    I edited the header
    There some changes i made to the header, footer and images

--- inc/footer.php
+++ inc/footer.php
@@ -130,7 +130,7 @@
             <span class="mobile-nav__close mobile-nav__toggler"><i class="far fa-times"></i></span>
 
             <div class="logo-box">
-                <a href="index.html" aria-label="logo image"><img src="assets/images/logo-light.png" width="155" alt="" /></a>
+                <a href="index.html" aria-label="logo image"><img src="../assets/images/Macagrofarm-white-logo.png" width="155" alt="" /></a>
             </div>
             <!-- /.logo-box -->
             <div class="mobile-nav__container"></div>
@@ -139,30 +139,21 @@
             <ul class="mobile-nav__contact list-unstyled">
                 <li>
                     <i class="agrikon-icon-email"></i>
-                    <a href="mailto:needhelp@agrikon.com">needhelp@agrikon.com</a>
+                    <a href="info@macagrofarms.com">info@macagrofarms.com</a>
                 </li>
                 <li>
                     <i class="agrikon-icon-telephone"></i>
-                    <a href="tel:666-888-0000">666 888 0000</a>
+                    <a href="tel:tel:+234(0)9032597686">09032597686</a>
                 </li>
             </ul>
             <!-- /.mobile-nav__contact -->
             <div class="mobile-nav__top">
-                <div class="mobile-nav__language">
-                    <img src="assets/images/resources/flag-1-1.jpg" alt="">
-                    <label class="sr-only" for="language-select">select language</label>
-                    <!-- /#language-select.sr-only -->
-                    <select class="selectpicker" id="language-select">
-                        <option value="english">English</option>
-                        <option value="arabic">Arabic</option>
-                    </select>
-                </div>
+            
                 <!-- /.mobile-nav__language -->
                 <div class="mobile-nav__social">
-                    <a href="#" aria-label="twitter"><i class="fab fa-twitter"></i></a>
-                    <a href="#" aria-label="facebook"><i class="fab fa-facebook-square"></i></a>
-                    <a href="#" aria-label="pinterest"><i class="fab fa-pinterest-p"></i></a>
-                    <a href="#" aria-label="instagram"><i class="fab fa-instagram"></i></a>
+                    <a href="www.facebook.com/macagrofarms" aria-label="facebook"><i class="fab fa-facebook-square"></i></a>
+                    <a href="www.linkedin.com/macagrofarms" aria-label="Linkedin"><i class="fab fa-linkedin"></i></a>
+                    <a href="www.instagram.com/macagrofarms" aria-label="instagram"><i class="fab fa-instagram"></i></a>
                 </div>
                 <!-- /.mobile-nav__social -->
             </div>
